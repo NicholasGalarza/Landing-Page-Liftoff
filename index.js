626c1d8252fccb1233d655274f5d7c32fff7c98f
Nicholas Liu <hkai.liu@gmail.com>
2017-09-29 13:01:05 -0400

    workshop completed at Bonus 2

--- index.js
+++ index.js
@@ -3,6 +3,23 @@ const logo = document.getElementById('start-logo');
 
 
 window.addEventListener("scroll", function() {
+  const sections = Array.from(document.querySelectorAll('section'));
+  const carouselImages = document.querySelectorAll(".carousel-image");
+  const rects = sections.map(section => section.getBoundingClientRect());
+  for (let i = 0; i < sections.length; i++) {
+    const section = sections[i];
+    const rect = rects[i];
+  
+    if (rect.top < window.innerHeight) {
+      section.classList.add('active');
+    } else {
+      section.classList.remove('active');
+    }
+  }
+  
+  carouselImages.forEach(image => {
+    image.style.???=???;
+  })
   const scrollPosition = window.scrollY;
   if (scrollPosition > 65) header.classList.add("active");
   else header.classList.remove('active');
